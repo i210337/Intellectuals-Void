919cc3a6eebfe3ee493db38bbe07bb39e30b3023
i210337 <i210337@nu.edu.pk>
2022-12-02 16:22:11 +0500

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,15 +1,31 @@
-// var MATHS_JS = {};
-// MATHS_JS.makeMultiplier = function (multiplier){
-// 	return(
-// 			function multiply(x){
-// 				return multiplier * x;
-// 			}
-// 		)
-// }
+var MATHS_JS = {};
+MATHS_JS.makeMultiplier = function (multiplier){
+	return(
+			function multiply(x){
+				return multiplier * x;
+			}
+		)
+}
+MATHS_JS.multiplyby3 = MATHS_JS.makeMultiplier(3);
+console.log(MATHS_JS.multiplyby3(5));
 
-// MATHS_JS.multiplyby3 = MATHS_JS.makeMultiplier(3);
-// console.log(MATHS_JS.multiplyby3(5));
+function sayHello () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Hello " + name + "!</h2>";
 
-console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
-console.log(2+"2")
-console.log(2-"2")
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+  if (name === "student") {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title += " & Lovin' it!";
+    document
+        .querySelector("h1")
+        .textContent = title;
+  }
+}
